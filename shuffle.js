367d12317763c0a34223216caf35b968af6180a5
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2023-02-09 23:47:02 +0500

    Add new shuffle function

---
+++ shuffle.js
@@ -0,0 +1,10 @@
+const shuffle = (array) => {
+  const shuffledArray = array
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
+
+  return shuffledArray
+}
+
+console.log(shuffle([1,2,3,4,5]))
